8f6d7b28e7f845858ac81990c49fecfa7214c133
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-06 22:10:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS!/n"
+    "Welcome to your app!!!! I BUILT THIS!"
     "This is a change I made."
   end
 
